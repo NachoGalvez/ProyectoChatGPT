61f833642606f882eaf4d010230bee114321fd6b
NachoGalvez <igalvezg@gmail.com>
2024-09-23 16:23:15 -0300

    Create API-Key

--- Test.py
+++ Test.py
@@ -1,1 +1,15 @@
-print("hola mundo")
+from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Escribe 1 parrafo de lo que quieras en español."
+        }
+    ]
+)
+
+print(completion.choices[0].message)
